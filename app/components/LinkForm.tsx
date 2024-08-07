ee45d26e09797fa3c7e935efd16c4db5b950f50a
Goldyn19 <nwaezegolden2002@gmail.com>
2024-08-07 09:20:02 +0100

    renders the links in mobile view tab

--- app/components/LinkForm.tsx
+++ app/components/LinkForm.tsx
@@ -1,37 +1,36 @@
 import React, { useState } from "react";
 import Image from "next/image";
 
-
 interface Option {
-    value: string;
-    icon: string;
-    color:string;
-  }
-  interface LinkFormProps {
-    id: number;
-  }
+  value: string;
+  icon: string;
+  color: string;
+}
 
-  const LinkForm: React.FC<LinkFormProps> = ({ id }) => {
-  const options:Option[] = [
-    { value: "GitHub", icon: "/images/githubicon.svg", color:'#1A1A1A' },
-    { value: "Frontend Mentor", icon: "/images/frontendmentor.svg", color:'#FFFFFF' },
-    { value: "Twitter", icon: "/images/twitter.svg", color:'#43B7E9' },
-    { value: "Linkedin", icon: "/images/linkedin.svg", color:'#2D68FF' },
-    { value: "Youtube", icon: "/images/youtube.svg", color:'#EE3939' },
-    { value: "Facebook", icon: "/images/facebook.svg", color:'#2442AC' },
-    { value: "twitch", icon: "/images/twitch.svg", color:'#EE3FC8' },
-    { value: "dev.to", icon: "/images/devto.svg", color:'#333333' },
-    { value: "Codewars", icon: "/images/codewars.svg", color:'#8A1A50' },
-    { value: "Codepen", icon: "/images/codepen.svg", color:'#1A1A1A' },
-    { value: "freeCodeCamp", icon: "/images/freecodecamp.svg", color:'#302267' },
-    { value: "GitLab", icon: "/images/gitlab.svg", color:'#EB4925' },
-    { value: "Hashnode", icon: "/images/hashnode.svg", color:'#0330D1' },
-    { value: "Stack Overflow", icon: "/images/stackoverflow.svg", color:'#EC7100' },
+interface LinkFormProps {
+  id: number;
+  updateLink: (id: number, option: Option) => void;
+}
 
+const LinkForm: React.FC<LinkFormProps> = ({ id, updateLink }) => {
+  const options: Option[] = [
+    { value: "GitHub", icon: "/images/githubicon.svg", color: "#1A1A1A" },
+    { value: "Frontend Mentor", icon: "/images/frontendmentor.svg", color: "#FFFFFF" },
+    { value: "Twitter", icon: "/images/twitter.svg", color: "#43B7E9" },
+    { value: "Linkedin", icon: "/images/linkedin.svg", color: "#2D68FF" },
+    { value: "Youtube", icon: "/images/youtube.svg", color: "#EE3939" },
+    { value: "Facebook", icon: "/images/facebook.svg", color: "#2442AC" },
+    { value: "twitch", icon: "/images/twitch.svg", color: "#EE3FC8" },
+    { value: "dev.to", icon: "/images/devto.svg", color: "#333333" },
+    { value: "Codewars", icon: "/images/codewars.svg", color: "#8A1A50" },
+    { value: "Codepen", icon: "/images/codepen.svg", color: "#1A1A1A" },
+    { value: "freeCodeCamp", icon: "/images/freecodecamp.svg", color: "#302267" },
+    { value: "GitLab", icon: "/images/gitlab.svg", color: "#EB4925" },
+    { value: "Hashnode", icon: "/images/hashnode.svg", color: "#0330D1" },
+    { value: "Stack Overflow", icon: "/images/stackoverflow.svg", color: "#EC7100" }
   ];
 
   const [selectedOption, setSelectedOption] = useState<Option>(options[0]);
-
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -41,6 +40,7 @@ interface Option {
   const selectOption = (option: Option) => {
     setSelectedOption(option);
     setDropdownOpen(false);
+    updateLink(id, option);
   };
 
   return (
@@ -59,13 +59,13 @@ interface Option {
         <h1 className="text-body-m text-light-black">Remove</h1>
       </div>
       <div className="relative mx-auto mt-4">
-        <label htmlFor="custom-dropdown " className="block mb-2 text-body-s">
+        <label htmlFor="custom-dropdown" className="block mb-2 text-body-s">
           Platform
         </label>
         <button
           id="custom-dropdown"
           onClick={toggleDropdown}
-          className="rounded-lg  w-full p-2.5 border border-light-black text-left flex justify-between items-center"
+          className="rounded-lg w-full p-2.5 border border-light-black text-left flex justify-between items-center"
         >
           <div className="flex items-center">
             <Image
@@ -87,11 +87,7 @@ interface Option {
               dropdownOpen ? "rotate-180" : ""
             }`}
           >
-            <path
-              d="M1 1L7 7L13 1"
-              stroke="#633CFF"
-              strokeWidth="2"
-            />
+            <path d="M1 1L7 7L13 1" stroke="#633CFF" strokeWidth="2" />
           </svg>
         </button>
         {dropdownOpen && (
@@ -115,34 +111,26 @@ interface Option {
           </div>
         )}
       </div>
-      <label
-            htmlFor="email-address-icon"
-            className="block text-body-s mb-[-10px]"
-          >
-            Link
-          </label>
-          <div className="relative w-full">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-            <Image
-                  src='/images/link.svg'
-                  alt='link image'
-                  className="h-auto w-auto"
-                  height={16}
-                  width={17}
-                />
-                <path
-                  d="M13 0H1C0.867392 0 0.740215 0.0526785 0.646447 0.146447C0.552678 0.240215 0.5 0.367392 0.5 0.5V9C0.5 9.26522 0.605357 9.51957 0.792893 9.70711C0.98043 9.89464 1.23478 10 1.5 10H12.5C12.7652 10 13.0196 9.89464 13.2071 9.70711C13.3946 9.51957 13.5 9.26522 13.5 9V0.5C13.5 0.367392 13.4473 0.240215 13.3536 0.146447C13.2598 0.0526785 13.1326 0 13 0ZM12.5 9H1.5V1.63688L6.66187 6.36875C6.75412 6.45343 6.87478 6.50041 7 6.50041C7.12522 6.50041 7.24588 6.45343 7.33813 6.36875L12.5 1.63688V9Z"
-                  fill="#737373"
-                />
-              
-            </div>
-            <input
-              type="text"
-              id="email-address-icon"
-              className=" rounded-lg block w-full pl-10  border border-dark-grey text-body-m"
-              placeholder="e.g. alex@email.com"
-            />
-          </div>
+      <label htmlFor="email-address-icon" className="block text-body-s mb-[-10px]">
+        Link
+      </label>
+      <div className="relative w-full">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3">
+          <Image
+            src="/images/link.svg"
+            alt="link image"
+            className="h-auto w-auto"
+            height={16}
+            width={17}
+          />
+        </div>
+        <input
+          type="text"
+          id="email-address-icon"
+          className="rounded-lg block w-full pl-10 border border-dark-grey text-body-m"
+          placeholder="e.g. alex@email.com"
+        />
+      </div>
     </div>
   );
 };
--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,59 +1,103 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import Navbar from '../components/navbar';
-import Image from 'next/image';
-import Emptylink from '../components/emptylink';
-import LinkForm from '../components/LinkForm';
+import React, { useState } from "react";
+import Navbar from "../components/navbar";
+import Image from "next/image";
+import Emptylink from "../components/emptylink";
+import LinkForm from "../components/LinkForm";
 
 type Link = {
   id: number;
+  value: string;
+  icon: string;
+  color: string;
 };
 
-const Page = () => {
- 
+const Page: React.FC = () => {
   const [links, setLinks] = useState<Link[]>([]);
 
   const addNewLink = () => {
-    setLinks([...links, { id: links.length + 1 }]);
+    setLinks([
+      ...links,
+      { id: links.length + 1, value: "GitHub", icon: "/images/githubicon.svg", color: "#1A1A1A" }
+    ]);
   };
+
+  const updateLink = (id: number, selectedOption: { value: string; icon: string; color: string }) => {
+    setLinks((prevLinks) =>
+      prevLinks.map((link) => (link.id === id ? { ...link, ...selectedOption } : link))
+    );
+  };
+
+  const renderLinks = () => {
+    const filledLinks = [
+      ...links,
+      ...Array(Math.max(0, 5 - links.length)).fill({
+        id: -1,
+        value: "",
+        icon: "",
+        color: "#E5E5E5"
+      })
+    ];
+
+    return filledLinks.map((link, index) => (
+      <div
+        key={index}
+        className="bg-[#EEEEEE] rounded-lg h-[45px] w-[240px] z-10 flex items-center justify-between px-2 "
+        style={{ backgroundColor: link.color }}
+      >
+        <div className="flex text-body-s text-white">
+        {link.icon && <Image src={link.icon} alt={link.value} width={16} height={16} className=""/>}
+        <span className="ml-2">{link.value}</span>
+        </div>
+        <div></div>
+      </div>
+    ));
+  };
+
   return (
-    <div className='p-0'>
+    <div className="p-0">
       <Navbar />
-      <div className='lg:grid lg:grid-cols-5 block h-full mt-3'>
-        <div className='hidden lg:flex justify-center col-span-2 w-full h-full bg-white mx-auto items-center relative'>
+      <div className="lg:grid lg:grid-cols-5 block h-full mt-3">
+        <div className="hidden lg:flex justify-center col-span-2 w-full h-full bg-white mx-auto items-center relative">
           <Image
             src="/images/mobile-preview.svg"
-            alt="logo"
+            alt="Mobile Preview"
             className="h-auto w-auto"
             height={632}
             width={308}
           />
-          <div className='bg-light-grey rounded-full h-[100px] w-[100px] z-10 flex justify-between absolute top-32'></div>
-          <div className='bg-light-grey rounded-lg h-[16px] w-[160px] z-10 flex justify-between absolute top-60 mt-3 '></div>
-          <div className='bg-light-grey rounded-lg h-[8px] w-[72px] z-10 flex justify-between absolute top-72  '></div>
-          <div className='bg-light-grey rounded-lg z-10 flex justify-between absolute'></div>
+          <div className='bg-[#EEEEEE] rounded-full h-[100px] w-[100px] z-10 flex justify-between absolute top-32'></div>
+          <div className='bg-[#EEEEEE] rounded-lg h-[16px] w-[160px] z-10 flex justify-between absolute top-60 mt-3 '></div>
+          <div className='bg-[#EEEEEE] rounded-lg h-[8px] w-[72px] z-10 flex justify-between absolute top-72  '></div>
+          <div className="p-5 space-y-4 z-10 flex flex-col justify-between absolute bottom-24">
+            {renderLinks()}
+          </div>
         </div>
-        <div className='px-5 bg-white col-span-3 ml-2 pb-24 relative'>
-          <h1 className='text-heading-m pt-5'>Customize your links</h1>
-          <h1 className='text-body-m text-light-black'>Add/edit/remove links below and then share all your profiles with the world!</h1>
+        <div className="px-5 bg-white col-span-3 ml-2 pb-24 relative">
+          <h1 className="text-heading-m pt-5">Customize your links</h1>
+          <p className="text-body-m text-light-black">
+            Add/edit/remove links below and then share all your profiles with the world!
+          </p>
           <button
-            className='text-heading-s text-custom-blue border border-custom-blue w-full rounded-lg mt-8'
+            className="text-heading-s text-custom-blue border border-custom-blue w-full rounded-lg mt-8"
             onClick={addNewLink}
           >
-            +Add new link
+            + Add new link
           </button>
-          <div className='pt-5 lg:h-[400px] overflow-y-scroll scrollbar-hide '>
-            {links.map(link => (
-              <LinkForm key={link.id} id={link.id}/>
+          <div className="pt-5 lg:h-[400px] overflow-y-scroll scrollbar-hide">
+            {links.map((link) => (
+              <LinkForm key={link.id} id={link.id} updateLink={updateLink} />
             ))}
             {links.length === 0 && <Emptylink />}
           </div>
-          <hr className='w-full mt-8 mb-5 border-light-black' />
-          <button className='bg-light-purple py-4 rounded-lg px-5 absolute right-5'>save</button>
+          <hr className="w-full mt-8 mb-5 border-light-black" />
+          <button className="bg-light-purple py-4 rounded-lg px-5 absolute right-5">
+            Save
+          </button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Page;
